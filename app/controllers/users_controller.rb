d0c58cffaa57f12ec933800a4bb5b338172f4cb1
EC2 Default User <ec2-user@ip-172-31-47-184.us-west-2.compute.internal>
2021-05-17 02:57:52 +0000

    051711:57

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
   
-  
+  def index
+    @users = User.all
+  end
+
   def show
     @user = current_user
   end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,5 +5,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_show_url
     assert_response :success
   end
+  
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
 
 end
