527eef6d06be6bc89fbc451b6f20f0ca1cb5089e
EC2 Default User <ec2-user@ip-172-31-47-184.us-west-2.compute.internal>
2021-05-28 02:38:45 +0000

    052811:38

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   get 'users/show' =>'users#show'
   get 'users', to: 'users#index'
     devise_for :users, controllers: {
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_054202) do
+ActiveRecord::Schema.define(version: 2021_05_28_023726) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
