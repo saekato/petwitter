25b6d945103234190a230e1ac2e01447cfa23eff
EC2 Default User <ec2-user@ip-172-31-47-184.us-west-2.compute.internal>
2021-05-31 08:17:32 +0000

    053117:17

--- db/migrate/20210528023726_create_articles.rb
+++ db/migrate/20210528023726_create_articles.rb
@@ -1,6 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
+      #外部ｷｰ（ﾃｰﾌﾞﾙ同士の紐づけ）
       t.references :user, foreign_key: true
       t.string :title
 
