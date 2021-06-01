04254a51f39f8609d2975ac9546af0836dc7821e
EC2 Default User <ec2-user@ip-172-31-47-184.us-west-2.compute.internal>
2021-06-01 02:43:54 +0000

    060111:43mypage完成

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,4 +1,11 @@
 class MypageController < ApplicationController
+    before_action :authenticate_user!
+    
     def index
-end
+        # publicの記事のみ表示
+    # ページネーションをつけたいデータに.page(params[:page])を追加
+    @articles = Article.where(user_id: current_user.id).page(params[:page]).per(10)
+    end
+    
+    
 end
