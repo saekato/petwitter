f6b4fdfcf1d58f94aac9c52f59c6978affec93d6
saekato <sassasssassssae@gmail.com>
2021-06-20 08:08:33 +0000

    0619レッスン

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,24 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
+  if Rails.env.production? || Rails.env.development?
   config.fog_provider = 'fog/aws' #required
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_S3_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
-    region: 'us-east-2'
+    region: 'ap-northeast-1'
     #host:                  's3.example.com',             # optional, defaults to nil
     #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
- 
-  config.fog_directory  = 'petwitter'                # required
+  config.asset_host = 'https://s3-us-west-2.amazonaws.com/petwitter111'
+  config.fog_directory  = 'petwitter111'                # required
   config.fog_public     = false                                                 # optional, defaults to true
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}"} # optional, defaults to {}
+ 
+
 end
+end
+
