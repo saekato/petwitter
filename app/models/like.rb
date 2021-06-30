93346b66edbb0b18e1619be6160156f66a4538ba
saekato <sassasssassssae@gmail.com>
2021-06-30 02:33:51 +0000

    0630いいね機能

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :article
   belongs_to :user
+  
+  #user_idとarticle_idどちらも存在している
+  validates :user_id, {presence: true}
+  validates :article_id, {presence: true}
+  
   #人が１つの投稿に対して、１つしかいいねをつけられないようにする(自分の投稿含)
   validates_uniqueness_of :article_id, scope: :user_id
 end
