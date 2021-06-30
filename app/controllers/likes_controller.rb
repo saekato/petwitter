43a6432f3fa3e1378d0b95581f5cd640b883054f
saekato <sassasssassssae@gmail.com>
2021-06-30 02:34:33 +0000

    0630いいね機能再

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,7 @@ class LikesController < ApplicationController
     def create
     @like = current_user.likes.create(article_id: params[:article_id])
     redirect_back(fallback_location: root_path)
-    @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
-    @like.save
-    redirect_to("/posts/#{params[:post_id]}")
-  end
+    
   end
 
   def destroy
