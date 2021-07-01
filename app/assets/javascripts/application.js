2d436e709b47dac9ea3c6410facfa47d8bc71a08
saekato <sassasssassssae@gmail.com>
2021-07-01 02:32:28 +0000

    0710ﾌﾟﾚﾋﾞｭｰ実装JSへ書き換え

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,4 +22,17 @@
 //= require popper
 //= require bootstrap-sprockets  
 
-console.log('hoge')　# 追加
+$(document).on('turbolinks:load', function() {
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+      $('#img_prev').attr('src', e.target.result);
+            }
+        reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#article_img").change(function(){
+        readURL(this);
+    });
+  });
