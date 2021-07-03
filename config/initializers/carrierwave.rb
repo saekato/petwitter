a91c7874f6ac8b823c2c8fbcc158925c76155247
saekato <sassasssassssae@gmail.com>
2021-07-03 02:39:51 +0000

    0702env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws' #required
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_S3_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
+    aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
     region: 'ap-northeast-1'
     #host:                  's3.example.com',             # optional, defaults to nil
     #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
