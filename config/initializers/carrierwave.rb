22bfb44f19a9fe757ee20673935e6abaec481419
saekato <sassasssassssae@gmail.com>
2021-07-03 01:46:51 +0000

    0703carriwave修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,22 +3,23 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production? || Rails.env.development?
-  config.fog_provider = 'fog/aws' #required
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_S3_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1'
-    #host:                  's3.example.com',             # optional, defaults to nil
-    #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
-  }
-  config.asset_host = 'https://s3-us-west-2.amazonaws.com/petwitter111'
-  config.fog_directory  = 'petwitter111'                # required
-  config.fog_public     = false                                                 # optional, defaults to true
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}"} # optional, defaults to {}
- 
+  if Rails.env.production? || Rails.env.test?#開発とテストは今まで通り
+    config.storage = :file
+  elsif Rails.env.production? #本番はS3に保存する
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+# credentials下にaws_access_key_idとaws_secret_access_keyはあるよ
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'petwitter111'
+    config.asset_host = 'https://s3-us-west-2.amazonaws.com/petwitter111'
+    config.fog_public     = false                                                 # optional, defaults to true
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}"} # optional, defaults to {}
+  end
+end
 
-end
-end
 
